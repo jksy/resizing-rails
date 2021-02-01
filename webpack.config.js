764c66eeaea81b99460c996958c4a6274b356d6f
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2021-02-02 00:32:30 +0900

    move webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './packs/resizing.js',
+  entry: './app/javascript/packs/resizing.js',
   output: {
     filename: 'resizing.bundle.js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, './app/javascript/dist')
   }
 };
