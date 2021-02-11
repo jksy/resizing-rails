c2b6d0031ababf92c1092b0b97a10d3662aab33e
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2021-02-12 02:18:48 +0900

    waiting ready state with js

--- app/assets/javascripts/resizing/rails/video.js
+++ app/assets/javascripts/resizing/rails/video.js
@@ -5,41 +5,49 @@ window.Resizing ||= {}
 window.Resizing.Rails ||= {}
 
 class Video {
-  constructor(self_url, style, element) {
+  constructor(self_url, style, parentElement) {
     this.self_url = self_url
     this.style = style
-    this.element = element
+    this.parentElement = parentElement
+    let video = this.buildVideoTag(style)
+    this.video = videojs(video.id)
   }
 
   fetch() {
     fetch(this.self_url, {method: 'GET', credentials: 'same-origin', headers: {'Content-Type': 'application/json'}})
     .then(response => response.json())
     .then(data => {
-      let template = document.getElementById('video-template')
-      let node = document.importNode(template.content, true)
-      let video = node.querySelector('video')
-      video.id = data.id
-      video.poster = data.thumbnail_url
-      this.addSourceToVideo(video, data.m3u8_url, 'application/x-mpegURL')
-      this.addSourceToVideo(video, data.avc_url, 'video/mp4')
-
-      if(this.style) {
-        video.setAttribute("style", this.style)
+      if(data.thumbnail_url) {
+        this.renderVideo(data)
       }
-      this.element.appendChild(video)
+      if(data.state != 'ready') {
+        setTimeout(this.fetch.bind(this), 30_000)
+      }
     }
     )
   }
 
-  addSourceToVideo(video, url, contentType) {
-    if(url === null || url === undefined) {
-      return
-    }
+  renderVideo(data) {
+    this.video.poster(data.thumbnail_url)
+    this.video.src({type: 'application/x-mpegURL', src: data.m3u8_url})
+    this.video.src({type: 'video/mp4', src: data.avc_url})
+  }
 
-    let source = document.createElement('source')
-    source.src = url
-    source.type = contentType
-    video.appendChild(source)
+  buildVideoTag() {
+    let template = document.getElementById('video-template')
+    let node = document.importNode(template.content, true)
+    let video = node.querySelector('video')
+    video.id = `video-${this.generateUniqueId()}`
+
+    if(this.style) {
+      video.setAttribute("style", this.style)
+    }
+    this.parentElement.appendChild(video)
+    return video
+  }
+
+  generateUniqueId() {
+    return (new Date).getTime().toString(16)
   }
 }
 
