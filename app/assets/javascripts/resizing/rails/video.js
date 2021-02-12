72ce13fe4dce6af1a416cbf8f11f996f954e6dc4
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2021-02-13 04:07:12 +0900

    fetch latest data from resizing for showing thumbnails

--- app/assets/javascripts/resizing/rails/video.js
+++ app/assets/javascripts/resizing/rails/video.js
@@ -14,17 +14,16 @@ class Video {
   }
 
   fetch() {
-    fetch(this.self_url, {method: 'GET', credentials: 'same-origin', headers: {'Content-Type': 'application/json'}})
-    .then(response => response.json())
-    .then(data => {
+    let fetcher = new Resizing.Rails.VideoFetcher(this.self_url)
+    fetcher.fetch().then(data => {
       if(data.thumbnail_url) {
         this.renderVideo(data)
       }
       if(data.state != 'ready') {
         setTimeout(this.fetch.bind(this), 30_000)
       }
-    }
-    )
+    })
+
   }
 
   renderVideo(data) {
@@ -34,9 +33,14 @@ class Video {
   }
 
   buildVideoTag() {
-    let template = document.getElementById('video-template')
-    let node = document.importNode(template.content, true)
-    let video = node.querySelector('video')
+    let video = document.createElement('video')
+    video.setAttribute('style', 'width: 500px; height: 300px;')
+    video.setAttribute('class', 'video-js')
+    video.setAttribute('muted', 'true')
+    video.setAttribute('controls', '')
+    video.setAttribute('preload', 'metadata')
+    video.setAttribute('data-setup', '{}')
+    video.setAttribute('poster', '')
     video.id = `video-${this.generateUniqueId()}`
 
     if(this.style) {
