7135ccbc06fd0b2c15faadf89c2e03c13fd6c918
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2021-02-28 23:09:58 +0900

    remove retundunt lines

--- app/assets/javascripts/resizing/rails.js
+++ app/assets/javascripts/resizing/rails.js
@@ -4,5 +4,4 @@
 //= require resizing/rails/video
 //= require resizing/rails/video_fetcher
 //= require resizing/rails/video_uploader
-//= require resizing/rails/video_uploader
 //= require resizing/rails/videojs/current/video.min
