5515f6b667cc3b22a3304de517bf9500cebaaf9b
Junichiro Kasuya <junichiro.kasuya@gmail.com>
2021-03-24 06:06:36 +0900

    add video loader. add some helper method. and bootstrap version is unfixed

--- app/assets/javascripts/resizing/rails.js
+++ app/assets/javascripts/resizing/rails.js
@@ -3,5 +3,6 @@
 //= require resizing/rails/toast
 //= require resizing/rails/video
 //= require resizing/rails/video_fetcher
+//= require resizing/rails/video_loader
 //= require resizing/rails/video_uploader
 //= require resizing/rails/videojs/7.8.2/video.min
